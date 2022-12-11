75cdc92e51bdefd41edf8fbc3a57e94527fd645c
ArseniZh <szhirnevich@mail.ru>
2022-12-11 22:44:56 +0300

    feat (Account): add fields id, cardNumber, name, blockAccount and constructor

--- src/account/Account.java
+++ src/account/Account.java
@@ -1,4 +1,16 @@
 package account;
 
 public class Account {
+    private int id;
+    private long cardNumber;
+    private String name;
+    private boolean blockAccount;
+
+    public Account(int id, long cardNumber, String name, boolean blockAccount) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.blockAccount = blockAccount;
+    }
+
 }
