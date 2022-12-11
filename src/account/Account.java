8b2b80329c963c4ffd3d86ddb2d80a532daabe91
ArseniZh <szhirnevich@mail.ru>
2022-12-12 00:32:01 +0300

    add (Account): add inner BillDetails class

--- src/account/Account.java
+++ src/account/Account.java
@@ -1,10 +1,13 @@
 package account;
 
+import java.util.ArrayList;
+
 public class Account {
     private int id;
     private long cardNumber;
     private String name;
     private boolean blockAccount;
+    private BillDetails billDetails = new BillDetails();
 
     public Account(int id, long cardNumber, String name, boolean blockAccount) {
         this.id = id;
@@ -13,6 +16,67 @@ public class Account {
         this.blockAccount = blockAccount;
     }
 
+    public class BillDetails {
+        private ArrayList<Double> withdrawals = new ArrayList<Double>(); //снятия
+        private ArrayList<Double> payments = new ArrayList<Double>(); //платежи
+        private ArrayList<Double> receipts = new ArrayList<Double>(); //поступления
+        private double monthlyWithdrawals;
+        private double monthlyPayments;
+        private double monthlyReceipts;
+
+        public void setWithdraw(double cash) {
+            withdrawals.add(cash);
+        }
+        public void setPayment(double cash) {
+            payments.add(cash);
+        }
+        public void setReceipt(double cash) {
+            receipts.add(cash);
+        }
+
+        public void calculateMonthlyWithdrawals() {
+            for(double withdrawal : withdrawals) {
+                monthlyWithdrawals += withdrawal;
+            }
+        }
+        public void calculateMonthlyPayments() {
+            for(double payment : payments) {
+                monthlyPayments += payment;
+            }
+        }
+        public void calculateMonthlyReceipts() {
+            for(double receipt : receipts) {
+                monthlyReceipts += receipt;
+            }
+        }
+        public double getMonthlyWithdrawals() {
+            calculateMonthlyWithdrawals();
+            return monthlyWithdrawals;
+        }
+        public double getMonthlyPayments() {
+            calculateMonthlyPayments();
+            return monthlyPayments;
+        }
+        public double getMonthlyReceipts() {
+            calculateMonthlyReceipts();
+            return monthlyReceipts;
+        }
+    }
+
+    public void withdrawMoney(double cash) {
+        billDetails.setWithdraw(cash);
+    }
+    public void makePayment(double cash) {
+        billDetails.setPayment(cash);
+    }
+    public void receiptMoney(double cash) {
+        billDetails.setReceipt(cash);
+    }
+    public String getStatistic() {
+        return "withdrawals per month: " + billDetails.getMonthlyWithdrawals() + " р."
+                + "\npayments per month: " + billDetails.getMonthlyPayments() + " р."
+                + "\nreceipts per month: " + billDetails.getMonthlyReceipts() + " р.";
+    }
 
     public void setId(int id) {
         this.id = id;
@@ -39,4 +103,10 @@ public class Account {
         return this.blockAccount;
     }
 
+    public String toString() {
+        return "id: " + this.id
+                + "\ncard number: " + this.cardNumber
+                + "\nname: " + this.name
+                + "\nblock account: " + this.blockAccount;
+    }
 }
