f0b39482d318e2b9cb794a621e99122c2f751b8e
ArseniZh <szhirnevich@mail.ru>
2022-12-12 00:33:32 +0300

    feat (Main): add objects of class Account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import account.Account;
+
 public class Main {
     public static void main(String[] args) {
+        Account account = new Account(115418, 1245634532, "Robin Hanson", false);
+        Account account1 = new Account(126489, 1248537732, "Amanda Robles", true);
 
+        account.receiptMoney(100);
+        account.makePayment(54.6);
+        account.receiptMoney(11.2);
+        System.out.println(account.getStatistic());
     }
 }
